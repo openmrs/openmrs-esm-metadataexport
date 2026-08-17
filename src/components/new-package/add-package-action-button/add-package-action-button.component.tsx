0642c10f2e9aa52ef8016a7c1a2b6b8a9c4d5f35
Bawanthathilan <bawantharathnayaka@gmail.com>
2026-08-17 23:11:34 +0530

    add + icon

--- src/components/new-package/add-package-action-button/add-package-action-button.component.tsx
+++ src/components/new-package/add-package-action-button/add-package-action-button.component.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@carbon/react';
+import { Add } from '@carbon/react/icons';
 import { launchAddNewPackageWorkspace } from '../new-package-utills';
 
 const NewPackageActionButton: React.FC = () => {
@@ -11,7 +12,12 @@ const NewPackageActionButton: React.FC = () => {
   }, [t]);
 
   return (
-    <Button onClick={handleAddNewPackageWorkspace} size="md" kind="primary">
+    <Button
+      onClick={handleAddNewPackageWorkspace}
+      size="md"
+      kind="primary"
+      renderIcon={(props) => <Add size={16} {...props} />}
+    >
       {t('newpackage', 'New Package')}
     </Button>
   );
